dbcd2388de6197fc986a5529199a4f39497d7236
Choi Geonu <6566gun@gmail.com>
2016-07-02 23:37:11 +0900

    Hide UpDown specific classes/structs under UpDownViewController's namespace

--- Example/RxMVC/UpDownViewController.swift
+++ Example/RxMVC/UpDownViewController.swift
@@ -13,53 +13,47 @@ import RxSwift
 import RxCocoa
 import RxMVC
 
-public enum UpDownEvent {
-    case ClickUp
-    case ClickDown
-    case ClickReset
-}
-
-enum UpDownAction {
-    case Increase
-    case Decrease
-    case Reset
-}
-
-struct UpDownController: MapController {
-    typealias Event = UpDownEvent
-    typealias Action = UpDownAction
-    func mapEventToAction(event: Event) -> Action {
-        switch event {
-        case .ClickUp:
-            return Action.Increase
-        case .ClickDown:
-            return Action.Decrease
-        default:
-            return Action.Reset
-        }
+class UpDownViewController: UIViewController, View, UserInteractable {
+    enum Event {
+        case ClickUp
+        case ClickDown
+        case ClickReset
     }
-}
-
-struct UpDownModel: ReducerModel {
-    typealias Action = UpDownAction
+    
+    enum Action {
+        case Increase
+        case Decrease
+        case Reset
+    }
+    
     typealias State = Int
     
-    let initialState = 0
-    func reduce(state: State, with action: Action) -> State {
-        switch action {
-        case .Increase:
-            return state + 1
-        case .Decrease:
-            return state - 1
-        case .Reset:
-            return initialState
+    struct UpDownController: MapController {
+        func mapEventToAction(event: Event) -> Action {
+            switch event {
+            case .ClickUp:
+                return Action.Increase
+            case .ClickDown:
+                return Action.Decrease
+            default:
+                return Action.Reset
+            }
         }
     }
-}
-
-public class UpDownViewController: UIViewController, View, UserInteractable {
-    public typealias Event = UpDownEvent
-    public typealias State = Int
+    
+    struct UpDownModel: ReducerModel {
+        let initialState = 0
+        func reduce(state: State, with action: Action) -> State {
+            switch action {
+            case .Increase:
+                return state + 1
+            case .Decrease:
+                return state - 1
+            case .Reset:
+                return initialState
+            }
+        }
+    }
     
     @IBOutlet weak var countLabel: UILabel!
     @IBOutlet weak var upButton: UIButton!
@@ -68,21 +62,21 @@ public class UpDownViewController: UIViewController, View, UserInteractable {
     
     var disposeBag = DisposeBag()
     
-    public func update(stateStream: Observable<State>) -> Disposable {
+    func update(stateStream: Observable<State>) -> Disposable {
         return stateStream.subscribeNext { (number) in
             self.countLabel.text = "\(number)"
         }
     }
     
-    public func interact() -> Observable<Event> {
+    func interact() -> Observable<Event> {
         return [
-            self.upButton.rx_tap.map{_ in UpDownEvent.ClickUp},
-            self.downButton.rx_tap.map{_ in UpDownEvent.ClickDown},
-            self.resetButton.rx_tap.map{_ in UpDownEvent.ClickReset},
+            self.upButton.rx_tap.map{_ in Event.ClickUp},
+            self.downButton.rx_tap.map{_ in Event.ClickDown},
+            self.resetButton.rx_tap.map{_ in Event.ClickReset},
             ].toObservable().merge()
     }
     
-    public override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         let model = UpDownModel()
         let view = self
@@ -91,7 +85,7 @@ public class UpDownViewController: UIViewController, View, UserInteractable {
         combineModel(model, withView: view, controller: controller, andUserInteractable: userInteractable).addDisposableTo(disposeBag)
     }
     
-    public override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
